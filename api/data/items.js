3115076f9eb3202bf9beb073e7eb74e53f5f87c2
ankit4548 <saimaparween281@gmail.com>
2024-09-07 03:26:29 +0530

    modified path

--- api/data/items.js
+++ api/data/items.js
@@ -2,7 +2,7 @@ const fs = require('node:fs/promises');
 const path = require('path')
 
 async function getStoredItems() {
-  const filepath = path.join(process.cwd(),'items.json')
+  const filepath = path.join(process.cwd(),'api/items.json')
   const rawFileContent = await fs.readFile(filepath, { encoding: 'utf-8' });
   const data = JSON.parse(rawFileContent);
   const storedItems = data.items ?? [];
