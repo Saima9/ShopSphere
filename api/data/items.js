dac890fc544c152b8afc22878cf4b9a49e833305
ankit4548 <saimaparween281@gmail.com>
2024-09-07 03:22:22 +0530

    added base path for local files

--- api/data/items.js
+++ api/data/items.js
@@ -1,14 +1,16 @@
 const fs = require('node:fs/promises');
+const path = require('path')
 
 async function getStoredItems() {
-  const rawFileContent = await fs.readFile('items.json', { encoding: 'utf-8' });
+  const filepath = path.join(process.cwd(),'items.json')
+  const rawFileContent = await fs.readFile(filepath, { encoding: 'utf-8' });
   const data = JSON.parse(rawFileContent);
   const storedItems = data.items ?? [];
   return storedItems;
 }
 
 function storeItems(items) {
-  return fs.writeFile('items.json', JSON.stringify({ items: items || [] }));
+  return fs.writeFile(filepath, JSON.stringify({ items: items || [] }));
 }
 
 exports.getStoredItems = getStoredItems;
