bcab47974e50863e0cf06cc587621ba0ed5f5b96
ankit4548 <saimaparween281@gmail.com>
2024-09-08 00:53:47 +0530

    updated cors paramters

--- api/index.js
+++ api/index.js
@@ -16,12 +16,12 @@ app.options('*', cors());  // Handle preflight requests for all routes
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 app.get('/items', async (req, res) => {
   const storedItems = await getStoredItems();
