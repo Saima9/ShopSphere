da8255c8b901a510d82ab998b5ba2c0080936e67
ankit4548 <saimaparween281@gmail.com>
2024-09-08 00:30:31 +0530

    updated api parameters

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  // res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
